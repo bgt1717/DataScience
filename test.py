1753a4780f861f2c3235a210c000a5478a3efdfb
bgt1717 <bgt17@txtstate.edu>
2024-12-29 22:35:38 -0600

    test6

--- test.py
+++ test.py
@@ -1,3 +1,15 @@
-msg = "hi"
+import matplotlib
+#Most of the Matplotlib utilities lies under the pyplot submodule, 
+# and are usually imported under the plt alias:
+import matplotlib.pyplot as plt
+import numpy as np
 
-print(msg)
+#Checking version of matplotlib.
+#print(matplotlib.__version__)
+
+#Draw a line in a diagram from position (0,0) to position (6,250):
+xpoints = np.array([0, 6])
+ypoints = np.array([0, 250])
+
+plt.plot(xpoints, ypoints)
+plt.show()
